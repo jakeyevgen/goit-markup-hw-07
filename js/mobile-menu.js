3a027a78bcd099f5cd9fcf7909b83af106405821
jakeyevgen <jakeyevgen1984@gmail.com>
2023-03-20 19:46:14 +0200

    Update 7

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-open');
-//   }
-// })();
 
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
