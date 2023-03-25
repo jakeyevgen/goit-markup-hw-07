00d9436d24f1d05d11027443b61042718e15383b
jakeyevgen <jakeyevgen1984@gmail.com>
2023-03-25 17:07:59 +0200

    Update 29

--- js/modal.js
+++ js/modal.js
@@ -11,4 +11,10 @@
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
+
+  function toggleModal() {
+  const body = document.querySelector('body');
+  body.classList.toggle('no-scroll');
+  refs.modal.classList.toggle('is-hidden');
+}
 })();
